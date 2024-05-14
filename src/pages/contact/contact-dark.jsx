5ec51fef6a4016490fd8bf48b88e065b49834a86
me-sajib <me.mrsajib@gmail.com>
2024-05-14 16:46:31 +0600

    add goggle map address

--- src/pages/contact/contact-dark.jsx
+++ src/pages/contact/contact-dark.jsx
@@ -6,58 +6,55 @@ import ContactForm from "components/Contact-form/contact-form";
 import DarkTheme from "layouts/Dark";
 
 const Contact = () => {
-  const navbarRef = React.useRef(null);
-  const logoRef = React.useRef(null);
+    const navbarRef = React.useRef(null);
+    const logoRef = React.useRef(null);
 
-  React.useEffect(() => {
-    document.querySelector("body").classList.add("contact-page");
+    React.useEffect(() => {
+        document.querySelector("body").classList.add("contact-page");
 
-    var navbar = navbarRef.current;
+        var navbar = navbarRef.current;
 
-    if (window.pageYOffset > 300) {
-      navbar.classList.add("nav-scroll");
-    } else {
-      navbar.classList.remove("nav-scroll");
-    }
+        if (window.pageYOffset > 300) {
+            navbar.classList.add("nav-scroll");
+        } else {
+            navbar.classList.remove("nav-scroll");
+        }
 
-    window.addEventListener("scroll", () => {
-      if (window.pageYOffset > 300) {
-        navbar.classList.add("nav-scroll");
-      } else {
-        navbar.classList.remove("nav-scroll");
-      }
-    });
+        window.addEventListener("scroll", () => {
+            if (window.pageYOffset > 300) {
+                navbar.classList.add("nav-scroll");
+            } else {
+                navbar.classList.remove("nav-scroll");
+            }
+        });
 
-    return () => {
-      document.querySelector("body").classList.remove("contact-page");
-    };
-  }, [navbarRef]);
+        return () => {
+            document.querySelector("body").classList.remove("contact-page");
+        };
+    }, [navbarRef]);
 
-  return (
-    <DarkTheme>
-      <Navbar nr={navbarRef} lr={logoRef} />
-      <ContactHeader />
-      <div className="main-content">
-        <ContactForm />
-        <div className="map" id="ieatmaps">
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6010.126439563913!2d-100.75478744312291!3d41.13314584948978!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54eab584e432360b%3A0x1c3bb99243deb742!2z2KfZhNmI2YTYp9mK2KfYqiDYp9mE2YXYqtit2K_YqQ!5e0!3m2!1sar!2seg!4v1644259110384!5m2!1sar!2seg"
-            loading="lazy"
-          ></iframe>
-        </div>
+    return (
+        <DarkTheme>
+            <Navbar nr={navbarRef} lr={logoRef} />
+            <ContactHeader />
+            <div className="main-content">
+                <ContactForm />
+                <div className="map" id="ieatmaps">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3984.0043861310314!2d101.64448027584426!3d3.0934911534904757!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31cc4bc778c46de5%3A0xedfb82b8230cb1aa!2s75%2C%20Jalan%20Templer%2C%20Pjs%206%2C%2046050%20Petaling%20Jaya%2C%20Selangor%2C%20Malaysia!5e0!3m2!1sen!2sbd!4v1715682432096!5m2!1sen!2sbd" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
 
-        <Footer hideBGCOLOR />
-      </div>
-    </DarkTheme>
-  );
+                <Footer hideBGCOLOR />
+            </div>
+        </DarkTheme>
+    );
 };
 
 export const Head = () => {
-  return (
-    <>
-      <title>Vie - Contact Dark</title>
-    </>
-  )
+    return (
+        <>
+            <title>Vie - Contact Dark</title>
+        </>
+    )
 }
 
 export default Contact;
